9e215751d94d08ef415178f6a8b0415a6fbe7d24
andrepinotti <andrepinotti0@gmail.com>
2025-07-04 17:28:07 -0300

    feat: applying the study of macros

--- create_macro/src/main.rs
+++ create_macro/src/main.rs
@@ -8,10 +8,20 @@ macro_rules! either {
         }
     }
 }
+
+macro_rules! sum {
+    ($a:expr, $b:expr) => {
+        $a+$b
+    };
+}
+
 // example of how to use macro
 fn main() {
   let foo = 1;
   let bar = 2;
   either!(foo == bar => println!("it is true"); println!("it is false"));
   either!(foo != bar => println!("it is true"); println!("it is false"));
+
+  let _x = sum!(4, 5);
+  println!("{}", _x)
 }
