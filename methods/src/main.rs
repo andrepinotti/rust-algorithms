c873d7035b9316ea400a562415f454092158c6f7
andrepinotti <andrepinotti0@gmail.com>
2025-07-04 13:51:19 -0300

    feat: applying methods in Rust

---
+++ methods/src/main.rs
@@ -0,0 +1,60 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: i32,
+    height: i32
+}
+
+impl Rectangle {
+    fn area(&self) -> i32{
+        &self.height * &self.width
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool{
+        self.width > other.width && self.height > other.height
+    }
+
+}
+
+fn main() {
+    
+    let scale = 2;
+    
+    let rec = Rectangle{
+        width: dbg!(30 * scale),
+        height: 30,
+    };
+
+    let rec_1=  Rectangle{
+        width: dbg!(5 * scale),
+        height: 7
+    };
+
+    let rec_2 = Rectangle {
+        width: dbg!(7 * scale),
+        height: 9
+    };
+
+    println!("RECTANGLES AND YOUR AREAS\n");
+
+    println!("{:?}", rec);
+    println!("Area of rec is: {:?}", rec.area());
+
+    println!("{:?}", rec_1);
+    println!("{:?}", rec_1.area());
+    
+    println!("{:?}", rec_2);
+    println!("{:?}", rec_2.area());
+
+    println!("\nCAN REC CONTAIN REC 1");
+    let x = rec.can_hold(&rec_1);
+    if x { println!("YES") } else { println!("NOT"); }
+
+    println!("\nCAN REC 1 CONTAIN REC 2");
+    let y = rec_1.can_hold(&rec_2);
+    if y { println!("YES"); } else { println!("NOT"); }
+
+    println!("\nCAN REC 2 CONTAIN REC 1");
+    let z = rec_2.can_hold(&rec_1);
+    if z { println!("YES"); } else { println!("NOT"); }
+
+}
